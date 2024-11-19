22574ab976de19daef1936ca43aaed1af10d4730
Jean <oka.jeanjaures@gmail.com>
2024-11-19 22:37:44 +0100

    Initial commit

---
+++ src/main/java/com/example/parisjanitormsattachment/ParisJanitorMsAttachmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.parisjanitormsattachment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParisJanitorMsAttachmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParisJanitorMsAttachmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parisjanitormsattachment/ParisJanitorMsAttachmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parisjanitormsattachment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParisJanitorMsAttachmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
