2cf741a932d28156424f8b32607a1dcfedb45202
Jean <oka.jeanjaures@gmail.com>
2025-02-05 23:47:19 +0100

    feat : Main class

--- src/main/java/com/example/parisjanitormsattachment/ParisJanitorMsAttachmentApplication.java
+++ src/main/java/com/example/parisjanitormsattachment/ParisJanitorMsAttachmentApplication.java
@@ -2,8 +2,12 @@ package com.example.parisjanitormsattachment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.parisjanitormsattachment")
+@EnableConfigurationProperties
 public class ParisJanitorMsAttachmentApplication {
 
     public static void main(String[] args) {
