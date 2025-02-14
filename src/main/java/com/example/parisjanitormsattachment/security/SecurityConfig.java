9ab7db1bc260675e13b6c45d7069aef139a20422
Jean <oka.jeanjaures@gmail.com>
2025-02-14 17:33:17 +0100

    feat(security): redefine security rules based on url patterns

--- src/main/java/com/example/parisjanitormsattachment/security/SecurityConfig.java
+++ src/main/java/com/example/parisjanitormsattachment/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 // Secure CORS Configuration
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api-docs","/swagger-ui/index.html").permitAll()
+                        .requestMatchers("/api/img/**","/api-docs","/swagger-ui/index.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults()); // Enable Basic Auth
