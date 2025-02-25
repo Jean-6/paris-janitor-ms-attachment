21110f5e3a059b995000ccb41340fff03b501b03
Jean <oka.jeanjaures@gmail.com>
2025-02-25 19:00:45 +0100

    perf (exception): Implement exception class

---
+++ src/main/java/com/example/parisjanitormsattachment/exception/RuntimeException.java
@@ -0,0 +1,7 @@
+package com.example.parisjanitormsattachment.exception;
+
+public class RuntimeException extends java.lang.RuntimeException {
+    public RuntimeException(String message) {
+        super(message);
+    }
+}
