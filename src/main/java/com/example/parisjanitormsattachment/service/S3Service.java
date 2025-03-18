c2da491f501feee6143f08c7cdaad1e62a5d37de
Jean <oka.jeanjaures@gmail.com>
2025-03-18 01:26:45 +0100

    refactor : update methods signature to reactor

--- src/main/java/com/example/parisjanitormsattachment/service/S3Service.java
+++ src/main/java/com/example/parisjanitormsattachment/service/S3Service.java
@@ -1,13 +1,15 @@
 package com.example.parisjanitormsattachment.service;
 
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface S3Service {
 
-    List<String> getImages(String propertyId);
-    String uploadImages(String propertyId,MultipartFile image);
-    List<String> uploadImages(String propertyId, List<MultipartFile> images);
+    Flux<String> getImages(String propertyId);
+    Mono<String> uploadImages(String propertyId, MultipartFile image);
+    Flux<String> uploadImages(String propertyId, List<MultipartFile> images);
 
 }
